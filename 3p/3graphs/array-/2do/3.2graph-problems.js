fe55290074821166a6961abbadf2ed763336dec1
juadi19 <juadi19@gmail.com>
2022-06-01 12:11:12 -0500

    Update 3.2graph-problems.js

--- 3p/3graphs/array-/2do/3.2graph-problems.js
+++ 3p/3graphs/array-/2do/3.2graph-problems.js
@@ -49,3 +49,80 @@
  * @see {@link https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array}
  * @see {@link https://jsdoc.app}
  */
+
+
+let tree1 = [
+  [0, 1, 0, 0], //1
+  [1, 0, 1, 1], //2
+  [0, 1, 0, 0], //3
+  [0, 1, 0, 0], //4
+];
+
+class Matriz {
+  constructor(matriz) {
+    this.matriz = matriz;
+  }
+
+  isSquare(matriz) {
+    for (let i = 0; i < matriz.length; i++) {
+      let evaluar;
+      evaluar = matriz[i].length;
+
+      if (evaluar != matriz.length) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  isSymmetrical(matriz) {
+    for (let i = 0; i < matriz.length; i++) {
+      for (let j = 0; j < matriz[i].length; j++) {
+        if (matriz[i][j] != matriz[j][i]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  isTree(matriz) {
+    let sum = 0;
+    let contador = 0;
+    let columnas = 0;
+
+    for (let i = 0; i < matriz.length; i++) {
+      for (let j = 0; j < matriz[i].length; j++) {
+        contador++;
+        columnas = contador / matriz.length;
+      }
+    }
+
+    for (let i = 1; i < matriz.length; i++) {
+      sum = 0;
+      for (let j = 0; j < columnas; j++) {
+        if (j < i) {
+          sum += matriz[i][j];
+          if (sum > 1) {
+            return false;
+          }
+        }
+      }
+    }
+    return true;
+  }
+
+  isGraph(matriz) {
+    if (matriz.isTree(matriz) == false) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+let matriz = new Matriz(tree1);
+console.log(matriz.isSquare(matriz));
+console.log(matriz.isSymmetrical(matriz));
+console.log(matriz.isTree(matriz));
+console.log(matriz.isGraph(matriz));
