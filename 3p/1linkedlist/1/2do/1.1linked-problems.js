869dd7b7540329a0e12a73c35f0095625650c120
juadi19 <juadi19@gmail.com>
2022-06-01 11:41:04 -0500

    Problem solved

--- 3p/1linkedlist/1/2do/1.1linked-problems.js
+++ 3p/1linkedlist/1/2do/1.1linked-problems.js
@@ -19,3 +19,63 @@
  * @see {@link https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array}
  * @see {@link https://jsdoc.app}
  */
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+
+  size() {
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count++;
+      node = node.next;
+    }
+    return count;
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  contains(v) {
+    let node = this.head;
+    while (node != null && node.data != v) node = node.next;
+    return node ? true : false;
+  }
+
+  getLast() {
+    let lastNode = this.head;
+    if (lastNode) {
+      while (lastNode.next) {
+        lastNode = lastNode.next;
+      }
+    }
+    return lastNode;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  insert(node) {
+    this.getLast().next = node;
+  }
+}
+
+class ListNode {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+let head = new ListNode(8);
+let node = new ListNode(2);
+head.next = node;
+let node1 = new ListNode("f");
+node.next = node1;
+
+let list = new LinkedList(head);
+
+console.log(list.contains("f"));
