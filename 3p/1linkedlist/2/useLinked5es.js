f87efeabee76c7eafed04a5f4cd8df4388f69583
juadi19 <juadi19@gmail.com>
2022-06-01 11:49:52 -0500

    Update useLinked5es.js

--- 3p/1linkedlist/2/useLinked5es.js
+++ 3p/1linkedlist/2/useLinked5es.js
@@ -7,9 +7,7 @@
 //let n1=new Node('a',new Node('f'))
 let n1=new Node('a')
 let n2=new Node('b')
-
 let l=new Linked(n1)
-l.traverse(l.getHead())
 
 l.prepend(n2)
 l.traverse(l.getHead())
@@ -23,5 +21,5 @@ l.prepend(new Node('d'))
 l.traverse(l.getHead())
 
 //then
-//console.log(l.contains('x'))//false
-//l.contains('a')//true
+console.log(l.contains('x'))//false
+l.contains('a')//true
